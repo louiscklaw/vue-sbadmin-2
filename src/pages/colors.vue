f67a93c40e4c120df266cac6f46434bb81d4fcf5
louiscklaw <louiscklaw@gmail.com>
2019-11-07 11:01:43 +0800

    update,

---
+++ src/pages/colors.vue
@@ -0,0 +1,22 @@
+<template>
+  <colors-content />
+</template>
+
+<script>
+  import colorsContent from '../components/colorsContent.vue'
+  import mainLayoutVue from '../layouts/mainLayout.vue'
+
+  export default {
+    components: {
+      'colors-content': colorsContent
+    },
+    created(){
+      this.$emit('update:layout', mainLayoutVue)
+    }
+  }
+
+</script>
+
+<style>
+
+</style>
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,22 +1,15 @@
 <template>
-  <index-inside></index-inside>
+  <index-content></index-content>
 </template>
 
 <script>
   import mainLayout from '../layouts/mainLayout.vue';
-
-
-
   import indexContent from '../components/indexContent.vue'
-
-  // import defaultCardExample from '../components/defaultCardExample.vue'
 
   export default {
     name: 'index',
     components: {
-
-
-      "index-inside": indexContent
+      "index-content": indexContent
     },
     created() {
       this.$emit( 'update:layout', mainLayout );
